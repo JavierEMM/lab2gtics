d6d268a1adb7b657c8d30b34d7cd78ccf20c04b5
Ana Sofia <alejandro.as@pucp.edu.pe>
2022-04-12 23:09:06 -0500

    inventario_controller_1

--- src/main/java/com/lab2gtics/controller/InventarioController.java
+++ src/main/java/com/lab2gtics/controller/InventarioController.java
@@ -1,4 +1,57 @@
 package com.lab2gtics.controller;
 
+import com.lab2gtics.entity.Inventario;
+import com.lab2gtics.repository.InventarioRepository;
+import com.lab2gtics.repository.MarcasRepository;
+import com.lab2gtics.repository.SedesRepository;
+import com.lab2gtics.repository.TiposRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+
+
+@Controller
+@RequestMapping("/inventario")
 public class InventarioController {
+    @Autowired
+    InventarioRepository inventarioRepository;
+    @Autowired
+    SedesRepository sedesRepository;
+    @Autowired
+    MarcasRepository marcasRepository;
+    @Autowired
+    TiposRepository tiposRepository;
+
+    @GetMapping("")
+    public String listaInventario(Model model){
+        model.addAttribute("listainventario", inventarioRepository.findAll());
+        return "Inventario/lista";
+    }
+
+    @GetMapping("/new")
+    public String crearInventario(Model model){
+        model.addAttribute("sede", sedesRepository.findAll());
+        model.addAttribute("marca", marcasRepository.findAll());
+        model.addAttribute("tipo", tiposRepository.findAll());
+
+        return "Inventario/nuevo";
+    }
+    @GetMapping("/edit")
+    public String editarInventario(Model model, RedirectAttributes attr, @RequestParam("id") Integer id){
+        Optional<Inventario> inventario = inventarioRepository.findById(id);
+        if (inventario.isPresent()){
+            model.addAttribute("inventario",inventario.get());
+            model.addAttribute("sede", sedesRepository.findAll());
+            model.addAttribute("marca", marcasRepository.findAll());
+            model.addAttribute("tipo", tiposRepository.findAll());
+            return "Inventario/editar";
+        }
+        return "redirect:/inventario";
+    }
 }

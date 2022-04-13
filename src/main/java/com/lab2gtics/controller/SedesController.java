830c0513ca634d5608eb60bed4e410ade3440cae
Javier Marroquin <a20193644@pucp.edu.pe>
2022-04-12 19:46:01 -0500

    listasedes.html

--- src/main/java/com/lab2gtics/controller/SedesController.java
+++ src/main/java/com/lab2gtics/controller/SedesController.java
@@ -1,14 +1,21 @@
 package com.lab2gtics.controller;
 
+import com.lab2gtics.repository.SedesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/sedes")
 public class SedesController {
-    @RequestMapping("")
-    public String listaSedes(){
+    @Autowired
+    SedesRepository sedesRepository;
 
-        return "Sedes";
+    @RequestMapping("")
+    public String listaSedes(Model model){
+
+        model.addAttribute("listasedes", sedesRepository.findAll());
+        return "Sedes/listasedes";
     }
 }

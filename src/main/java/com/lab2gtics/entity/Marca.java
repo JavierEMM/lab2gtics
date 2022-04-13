7b2ae6a657abcba529e3f3bf7a4b35947b96589b
Ana Sofia <alejandro.as@pucp.edu.pe>
2022-04-12 19:40:22 -0500

    conexion database

--- src/main/java/com/lab2gtics/entity/Marca.java
+++ src/main/java/com/lab2gtics/entity/Marca.java
@@ -1,4 +1,11 @@
 package com.lab2gtics.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tipo")
 public class Marca {
+    @Id
 }
